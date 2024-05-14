3736d525eae3e52bf8b84a23975528e1c8c2a4a4
barte <barte@LAPTOP-GA3N7E1H>
2024-05-14 19:30:16 +0200

    Dodaj pliki projektów.

---
+++ Blazorna4/Blazorna4/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Blazorna4.Components;
+
+namespace Blazorna4.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<Blazorna4.Components.Movie> Movie { get; set; } = default!;
+    }
+}
